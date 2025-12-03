18d855aa1d9726f2156d6ddb37fc812a0d7890a8
solomon-israel <solomonisraelayomide630@gmail.com>
2025-12-03 16:26:13 +0100

    styled the header well

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,24 @@ import { IoMdSearch } from "react-icons/io";
 export default function App() {
   return (
     <div className={styles.wrapper}>
-      <header>
-        <h5>Where in the world?</h5>
-        <div className={styles.themeSwitchContainer}>
-          <IoMoonOutline />
-          <span>Dark mode</span>
-        </div>
-      </header>
-      <div className={styles.body}>
-        <div className={styles.searContainer}>
-          {/* <IoMdSearch className={styles.searchIcon} /> */}
-          <input type="text" placeholder="Search for a country" />
+      <div>
+        <header>
+          <h5>Where in the world?</h5>
+          <div className={styles.themeSwitchContainer}>
+            <IoMoonOutline />
+            <span>Dark mode</span>
+          </div>
+        </header>
+        <div className={styles.body}>
+          <div className={styles.searchContainer}>
+            {/* <IoMdSearch className={styles.searchIcon} /> */}
+            <input type="text" placeholder="Search for a country" />
+          </div>
+          <div className={styles.dropDown}>
+            <div className={styles.dash}></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
